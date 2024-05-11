b5ecf7a49d5db4585cfb110c18bf62ab1dae83db
jieshen <jieshensherwinlee@gmail.com>
2024-05-12 01:06:32 +0530

    first commit

---
+++ app/src/main/java/com/example/labexam4/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.labexam4
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.labexam4.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    @SuppressLint("SuspiciousIndentation")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.addButton.setOnClickListener{
+            val intent = Intent(this, AddNoteActivity::class.java)
+            startActivity(intent)
+        }
+
+
+
+    }
+}
